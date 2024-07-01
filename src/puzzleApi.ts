08ea8e91c4237d3d02afeed8c8e9a25af03cac2e
Nate Grigg <n8mob@nategrigg.com>
2024-07-01 17:54:07 -0600

    forget caching for now (changing too often)

--- src/puzzleApi.ts
+++ src/puzzleApi.ts
@@ -4,17 +4,11 @@ import {Menu} from "./Menu.ts";
 const API_BASE_URL = 'http://puzzleeditor2020.local:62443/menus';
 
 export const getMenu = async (menuName: string): Promise<Menu> => {
-  const cachedMenu = localStorage.getItem(menuName);
-  if (!cachedMenu) {
-    try {
-      const response = await axios.get(`${API_BASE_URL}/${menuName}`);
-      localStorage.setItem(menuName, JSON.stringify(response.data));
-      return response.data;
-    } catch (error) {
-      console.error('Failed to fetch menu data:', error);
-      throw error;
-    }
-  } else {
-    return JSON.parse(cachedMenu);
+  try {
+    const response = await axios.get(`${API_BASE_URL}/${menuName}`);
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch menu data:', error);
+    throw error;
   }
 };
