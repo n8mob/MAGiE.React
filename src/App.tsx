414c600dc6c1d20df8c401cda489c9c9932526ca
Nate Grigg <n8mob@nategrigg.com>
2024-07-10 18:15:32 -0600

    hook up variable-width encoding

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import './App.css'
 import {useEffect, useState} from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {getMenu} from "./puzzleApi.ts";
-import {FixedEncodingData, Menu} from "./Menu.ts";
+import {FixedEncodingData, Menu, VariableEncodingData} from "./Menu.ts";
 import MenuDisplay from "./components/MenuDisplay.tsx";
 import LevelMenu from "./components/LevelMenu.tsx";
 import LevelPlay from "./components/LevelPlay.tsx";
 import {FixedWidthEncoder} from "./FixedWidthEncoder.ts";
+import {VariableWidthEncoder} from "./VariableWidthEncoder.ts";
 
 function App() {
   const [menu, setMenu] = useState<Menu | null>(null);
@@ -21,6 +22,9 @@ function App() {
             const fixedEncoding = encodingData as FixedEncodingData;
             menuData.encodingProviders[encodingName] = new FixedWidthEncoder(
               fixedEncoding.encoding.width, fixedEncoding.encoding.encodingMap);
+          } else if (encodingData.type == "variable") {
+            const variableEncoding = encodingData as VariableEncodingData;
+            menuData.encodingProviders[encodingName] = new VariableWidthEncoder(variableEncoding.encoding);
           }
         });
 
@@ -32,7 +36,6 @@ function App() {
               }
             });
           });
-
         });
         setMenu(menuData);
       } catch (error) {
