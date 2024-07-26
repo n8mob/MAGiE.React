0a4102c471b35198db78e34cd29fb3216881f01b
Nate Grigg <n8mob@nategrigg.com>
2024-07-26 16:39:27 -0600

    cache menu by name to local storage

--- src/puzzleApi.ts
+++ src/puzzleApi.ts
@@ -4,8 +4,14 @@ import {Menu} from "./Menu.ts";
 const API_BASE_URL = '/puzzle-api';
 
 export const getMenu = async (menuName: string): Promise<Menu> => {
+  const menuData = localStorage.getItem(menuName);
+  if (menuData) {
+    return JSON.parse(menuData);
+  }
+
   try {
     const response = await axios.get(`${API_BASE_URL}/${menuName}`);
+    localStorage.setItem(menuName, JSON.stringify(response.data));
     return response.data;
   } catch (error) {
     console.error('Failed to fetch menu data:', error);
