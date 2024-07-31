9581540e4d6a13c7fbe3ae8e74083406b6aacce9
Nate Grigg <n8mob@nategrigg.com>
2024-07-30 20:20:55 -0600

    Backup the old app as MenuApp.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import MenuApp from "./MenuApp.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MenuApp />
   </React.StrictMode>,
 )
