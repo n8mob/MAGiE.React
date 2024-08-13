989b5904cc979e2bae6e69bec540f32a2e6ec385
Nate Grigg <n8mob@nategrigg.com>
2024-08-13 16:49:59 -0600

    testing localization

--- src/components/DailyPuzzle.tsx
+++ src/components/DailyPuzzle.tsx
@@ -68,8 +68,31 @@ export default class DailyPuzzle extends Component<DailyPuzzleProps, DailyPuzzle
   }
 
   handleShareWin() {
-    const today = new Date().toLocaleDateString();
-    const shareText = `I decoded the MAGiE puzzle for ${today}!`;
+    const today = new Date();
+    const testLocales = [
+      'en-US',
+      'en-GB',
+      'fr-FR',
+      'de-DE',
+      'zh-CN',
+      'ja-JP',
+      'ar-SA',
+      'he-IL',
+      'ru-RU',
+      'es-MX',
+    ];
+
+    const prettyOptions: Intl.DateTimeFormatOptions = {weekday: 'short', month: 'long', day: 'numeric'};
+
+    testLocales.map(userLocale => {
+      const formattedDate = today.toLocaleDateString(userLocale, prettyOptions);
+      const shareText = `deployment test (${formattedDate})`;
+      console.log(shareText);
+    });
+
+    const userLocale = navigator.language;
+    const formattedDate = today.toLocaleDateString(userLocale, prettyOptions);
+    const shareText = `I decoded today's MAGiE puzzle! (${formattedDate})`;
     if (navigator.share) {
       navigator.share({
         title: "MAGiE binary puzzles",
@@ -80,7 +103,7 @@ export default class DailyPuzzle extends Component<DailyPuzzleProps, DailyPuzzle
     } else if (navigator.clipboard) {
       const shareViaClipboard =
         'It seems that this browser does not support "Web Share".'
-      + '\nShall we copy the share message to your clipboard?';
+        + '\nShall we copy the share message to your clipboard?';
       if (window.confirm(shareViaClipboard)) {
         navigator.clipboard.writeText(shareText)
           .catch(error => {
@@ -90,5 +113,5 @@ export default class DailyPuzzle extends Component<DailyPuzzleProps, DailyPuzzle
       }
     }
   }
-  }
+}
 
