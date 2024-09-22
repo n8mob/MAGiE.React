7aef45cb2279528a9c25987e11f29f156d32ce8e
Nate Grigg <n8mob@nategrigg.com>
2024-09-21 21:30:41 -0600

    allow access to yesterday's puzzle

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import ReactGA4 from 'react-ga4';
 import SpecificDaysPuzzle from "./components/SpecificDaysPuzzle.tsx";
 import TodaysPuzzle from "./components/TodaysPuzzle.tsx";
+import YesterdaysPuzzle from "./components/YesterdaysPuzzle.tsx";
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<TodaysPuzzle/>}/>
           <Route path="/test/:year/:month/:day" element={<SpecificDaysPuzzle/>}/>
+          <Route path="/yesterday" element={<YesterdaysPuzzle/>}/>
         </Routes>
       </Router>
     </>
