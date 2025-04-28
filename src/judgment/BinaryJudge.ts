f0fd3809d41ecae952a941e77c13ae15278a1a38
Nate Grigg <n8mob@nategrigg.com>
2025-04-28 11:27:18 -0600

    BaseBinaryJudge to avoid handling "null judge" in PuzzleState

--- src/judgment/BinaryJudge.ts
+++ src/judgment/BinaryJudge.ts
@@ -1,10 +1,30 @@
-import {CharJudgment, SequenceJudgment} from "./SequenceJudgment.ts";
-import FullJudgment from "./FullJudgment.ts";
-import { BitString, DisplayRow } from "../encoding/BinaryEncoder.ts";
+import { CharJudgment, SequenceJudgment } from "./SequenceJudgment.ts";
+import { FullJudgment } from "./FullJudgment.ts";
+import { BitSequence } from "../BitSequence.ts";
 
-export type SplitterFunction = (bits: BitString) => Generator<DisplayRow, void>;
+type SplitterFunction = (bits: BitSequence) => Generator<BitSequence, void>;
 
-export default interface BinaryJudge {
-  judgeBits<T extends SequenceJudgment>(guessBits: BitString, winBits: BitString, split: SplitterFunction): FullJudgment<T>;
+interface BinaryJudge {
+  judgeBits<T extends SequenceJudgment>(
+    guessBits: BitSequence,
+    winBits: BitSequence,
+    split: SplitterFunction
+  ): FullJudgment<T>;
+
   judgeText(guessText: string, winText: string): FullJudgment<CharJudgment>;
 }
+
+class BaseBinaryJudge implements BinaryJudge {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  judgeBits<T extends SequenceJudgment>(_guessBits: BitSequence, _winBits: BitSequence, _split: SplitterFunction): FullJudgment<T> {
+    throw new Error("judgeBits not implemented");
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  judgeText(_guessText: string, _winText: string): FullJudgment<CharJudgment> {
+    throw new Error("judgeText not implemented");
+  }
+}
+
+export type { BinaryJudge, SplitterFunction };
+export { BaseBinaryJudge };
