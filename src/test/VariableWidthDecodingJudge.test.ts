3985f861460c41f92e516e23ffdafef99211c604
Nate Grigg <n8mob@nategrigg.com>
2025-04-28 18:29:28 -0600

    refactor VariableWidthDecodingJudge to use BitSequence for bit operations

--- src/test/VariableWidthDecodingJudge.test.ts
+++ src/test/VariableWidthDecodingJudge.test.ts
@@ -1,73 +1,76 @@
-import {CharJudgment, DisplayRowJudgment} from "../judgment/SequenceJudgment.ts";
-import FullJudgment from "../judgment/FullJudgment.ts";
-import VariableWidthDecodingJudge from "../judgment/VariableWidthDecodingJudge.ts";
-import {beforeEach, describe, expect, it} from "vitest";
-import VariableWidthEncoder from "../encoding/VariableWidthEncoder.ts";
+import { CharJudgment, SequenceJudgment } from "../judgment/SequenceJudgment.ts";
+import { FullJudgment } from "../judgment/FullJudgment.ts";
+import { VariableWidthDecodingJudge } from "../judgment/VariableWidthDecodingJudge.ts";
+import { beforeEach, describe, expect, it } from "vitest";
+import { VariableWidthEncoder } from "../encoding/VariableWidthEncoder.ts";
+import { BitSequence } from "../BitSequence.ts";
 
 describe('VariableWidthDecodingJudge', () => {
+  let testEncoder: VariableWidthEncoder;
   let unitUnderTest: VariableWidthDecodingJudge;
-  const split = function* (bits: string) {
-    let b2 = bits;
+  const split = function* (bits: BitSequence) {
+    let b2: BitSequence = bits;
+    const sliceWidth = 13;
+    let sliceCount = 1;
     while (b2 && b2.length > 0) {
-      yield b2.slice(0, 13);
-      b2 = b2.slice(13);
+      const slice: BitSequence = b2.slice(0, sliceWidth * sliceCount++);
+      yield slice;
+      b2 = b2.slice(sliceWidth);
     }
-  }
+  };
 
   beforeEach(() => {
-    const encoder: VariableWidthEncoder = new VariableWidthEncoder({
-      "1": {
-        "A": "1",
-        "B": "11",
-        "C": "111"
-      },
-      "0": {
-        "": "0",
-        " ": "00",
-        ".": "000",
-        ". ": "00000"
+    testEncoder = new VariableWidthEncoder(
+      {
+        "1": {
+          "A": "1",
+          "B": "11",
+          "C": "111"
+        },
+        "0": {
+          "": "0",
+          " ": "00",
+          ".": "000",
+          ". ": "00000"
+        }
       }
-    });
+    );
 
-    unitUnderTest = new VariableWidthDecodingJudge(encoder);
+    unitUnderTest = new VariableWidthDecodingJudge(testEncoder);
   });
 
   it("should judge a single correct character", () => {
-    const guessBits = "1111";
-    const bitJudgments = "1111";
-    const winBits = "1111"
-    const rowJudgment = new DisplayRowJudgment(guessBits, bitJudgments);
+    const correctBitString = "1111";
+    const guessBits = BitSequence.fromString(correctBitString);
+    const winBits = BitSequence.fromString(correctBitString);
+    const fourCorrect = "1111";
+    const rowJudgment = new SequenceJudgment(guessBits, fourCorrect);
     const expected = new FullJudgment(
       true,
       guessBits,
       [rowJudgment]
     );
     const actual = unitUnderTest.judgeBits(guessBits, winBits, split);
-    expect(actual).toEqual(expected);
+    expect(actual).to.deep.equal(expected);
   });
 
+  // noinspection DuplicatedCode
   it("should judge a string of correct characters", () => {
     const guessText = "A CAB.";
     const winText = "A CAB.";
-    const expected = new FullJudgment(true, "A CAB.", [
-      new CharJudgment("1", "1"),
-      new CharJudgment("00", "11"),
-      new CharJudgment("111", "111"),
-      new CharJudgment("1", "1"),
-      new CharJudgment("11", "11"),
-      new CharJudgment("000", "111"),
-    ]);
+    const guessBits = testEncoder.encodeText(guessText);
     const actual = unitUnderTest.judgeText(guessText, winText);
-    expect(actual).toBeInstanceOf(FullJudgment<CharJudgment>);
-    // make sure getCharJudgments is not empty and has the right elements in it.
-    const expectedCharJudgments = expected.getCharJudgments();
-    const actualCharJudgments = actual.getCharJudgments();
-    let nextExpected = expectedCharJudgments.next();
-    let nextActual = actualCharJudgments.next();
-    while (!nextExpected.done) {
-      expect(nextActual.value).toEqual(nextExpected.value);
-      nextActual = actualCharJudgments.next();
-      nextExpected = expectedCharJudgments.next();
+    expect(actual).to.be.instanceof(FullJudgment<CharJudgment>);
+    expect(actual.isCorrect).to.be.true;
+    expect(actual.correctGuess.equals(guessBits)).to.be.true;
+    const charJudgments = actual.getCharJudgments();
+    let nextCharJudgment = charJudgments.next();
+    while(!nextCharJudgment.done) {
+      nextCharJudgment.value.bitJudgments.forEach(bitJudgment => {
+        expect(bitJudgment.isCorrect).to.be.true;
+      })
+      nextCharJudgment = charJudgments.next();
     }
   });
 });
+
