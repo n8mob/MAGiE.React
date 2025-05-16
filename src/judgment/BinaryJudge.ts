d094847cdbc55c2e6ca5819ede78227dc38654f2
Nate Grigg <n8mob@nategrigg.com>
2025-05-16 17:54:34 -0600

    move judgeBits into an abstract base class.

--- src/judgment/BinaryJudge.ts
+++ src/judgment/BinaryJudge.ts
@@ -1,30 +1,81 @@
 import { CharJudgment, SequenceJudgment } from "./SequenceJudgment.ts";
 import { FullJudgment } from "./FullJudgment.ts";
 import { BitSequence } from "../BitSequence.ts";
+import { BitJudgment } from "./BitJudgment.ts";
+import { IndexedBit } from "../IndexedBit.ts";
 
 type SplitterFunction = (bits: BitSequence) => Generator<BitSequence, void>;
+type NewSequenceJudgment<T extends SequenceJudgment> = (bits: BitSequence, judgments: string | BitJudgment[]) => T;
+type BitJudge = (guessBit?: IndexedBit, winBit?: IndexedBit) => BitJudgment;
 
 interface BinaryJudge {
   judgeBits<T extends SequenceJudgment>(
     guessBits: BitSequence,
     winBits: BitSequence,
-    split: SplitterFunction
+    split: SplitterFunction,
+    bitJudge: BitJudge,
+    newSequenceJudgment: NewSequenceJudgment<T>
   ): FullJudgment<T>;
 
   judgeText(guessText: string, winText: string): FullJudgment<CharJudgment>;
 }
 
-class BaseBinaryJudge implements BinaryJudge {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  judgeBits<T extends SequenceJudgment>(_guessBits: BitSequence, _winBits: BitSequence, _split: SplitterFunction): FullJudgment<T> {
-    return new FullJudgment(false, BitSequence.empty(), []);
+abstract class BaseBinaryJudge implements BinaryJudge {
+  judgeBits<T extends SequenceJudgment>(
+    guessBits: BitSequence,
+    winBits: BitSequence,
+    split: SplitterFunction,
+    bitJudge: BitJudge,
+    newSequenceJudgment: NewSequenceJudgment<T>
+  ): FullJudgment<T> {
+    const sequenceJudgments: T[] = [];
+    const guessSplit = split(guessBits);
+    const winSplit = split(winBits);
+    let nextGuess = guessSplit.next();
+    let nextWin = winSplit.next();
+    let allCorrect = true;
+    const correctBits: IndexedBit[] = [];
+
+    while (!nextWin.done) {
+      if (nextGuess.done) {
+        allCorrect = false;
+        const missingJudgments = new Array(nextWin.value.length)
+          .fill(bitJudge(undefined, nextWin.value.firstBit()));
+
+        sequenceJudgments.push(newSequenceJudgment(nextWin.value, missingJudgments));
+        nextWin = winSplit.next();
+        continue; // guess is done, if we were to advance it, it might even crash this thing!
+      }
+
+      const bitJudgments: BitJudgment[] = [];
+      for (const guessBit of nextGuess.value) {
+        const bitJudgment = bitJudge(guessBit, nextWin.value.getBitByGlobalIndex(guessBit.index));
+        bitJudgments.push(bitJudgment);
+        if (bitJudgment.isCorrect) {
+          correctBits.push(guessBit);
+        } else {
+          allCorrect = false;
+        }
+      }
+
+      sequenceJudgments.push(newSequenceJudgment(nextGuess.value, bitJudgments));
+      nextGuess = guessSplit.next();
+      nextWin = winSplit.next();
+    }
+
+    // Handle superfluous guesses
+    while (!nextGuess.done) {
+      allCorrect = false;
+      const bitJudgments = [...nextGuess.value].map(bit => bitJudge(bit, undefined));
+      sequenceJudgments.push(newSequenceJudgment(nextGuess.value, bitJudgments));
+      nextGuess = guessSplit.next();
+    }
+
+    return new FullJudgment(allCorrect, correctBits, sequenceJudgments);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  judgeText(_guessText: string, _winText: string): FullJudgment<CharJudgment> {
-    return new FullJudgment(false, BitSequence.empty(), []);
-  }
+  abstract judgeText(guessText: string, winText: string): FullJudgment<CharJudgment>;
 }
 
-export type { BinaryJudge, SplitterFunction };
-export { BaseBinaryJudge };
+export type { SplitterFunction, NewSequenceJudgment, BitJudge };
+export { BinaryJudge, BaseBinaryJudge };
