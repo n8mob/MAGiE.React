f742b6981fd53c2e99f7c84840abac33accecc44
Nate Grigg <n8mob@nategrigg.com>
2025-05-24 17:31:31 -0600

    wrap "first time" and settings in dialog components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
+// App.tsx
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ReactGA4 from 'react-ga4';
 import FirstTimeOverlay from "./components/FirstTimeOverlay.tsx";
 import SpecificDaysPuzzle from "./components/SpecificDaysPuzzle.tsx";
-import {usePageTracking} from "./hooks/usePageTracking.ts";
-import {useState} from "react";
+import { usePageTracking } from "./hooks/usePageTracking.ts";
+import { useState } from "react";
+import SettingsDialog from "./components/SettingsDialog.tsx";
+import Dialog from './components/Dialog.tsx';
 
 ReactGA4.initialize('G-ZL5RKDBBF6');
 
 function App() {
   usePageTracking();
-  const [overlayDismissed, setOverlayDismissed] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
+  const [showFirstTime, setShowFirstTime] = useState(true);
 
   return (
     <>
-      {!overlayDismissed && <FirstTimeOverlay onClose={() => setOverlayDismissed(true)}/>}
+      {showFirstTime &&
+        <Dialog onClose={() => setShowFirstTime(false)}>
+          <FirstTimeOverlay />
+        </Dialog>
+      }
+      {showSettings &&
+        <Dialog onClose={() => setShowSettings(false)}>
+          <SettingsDialog onClose={() => setShowSettings(false)}/>
+        </Dialog>
+      }
+
       <h1 id="magie-title">MAGiE</h1>
       <Routes>
         <Route path="/" element={<SpecificDaysPuzzle initialDate={new Date()}/>}/>
