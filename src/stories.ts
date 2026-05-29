3462e47cd8e9e99a1c9fe61c5778309512ed8e45
Nate Grigg <n8mob@nategrigg.com>
2026-05-29 15:19:31 -0600

    revise stories for first publication

--- src/stories.ts
+++ src/stories.ts
@@ -1,7 +1,5 @@
 export const stories = [
-  { slug: "01", fileName: "01.Open.md", title: "Open" },
-  { slug: "02", fileName: "02.PreparationPeriod.md", title: "Preparation Period" },
-  { slug: "03", fileName: "03.MetingUpWithHepi.md", title: "Meeting Up With Hepi" },
-  { slug: "04", fileName: "04.First_SignalHut_then_TheNewsstand.md", title: "First SignalHut, then The Newsstand" },
-  { slug: "10", fileName: "10.EnoughWaiting.md", title: "Enough Waiting" }
+
+  { slug: "01", fileName: "01.MagazinesAtTheNewsstand.md", title: "Magazines at the Newsstand" },
+  { slug: "02", fileName: "02.ChasingTheTrashBot.md", title: "Chasing the Trash Bot" },
 ];
