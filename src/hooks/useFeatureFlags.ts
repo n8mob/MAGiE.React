599f0273b81f5f84e26056ca69f16161ae24ec9f
Nate Grigg <n8mob@nategrigg.com>
2026-06-12 12:53:34 -0600

    Add 'date' feature to NORMAL_FEATURES array

--- src/hooks/useFeatureFlags.ts
+++ src/hooks/useFeatureFlags.ts
@@ -27,7 +27,7 @@ function getOrCreateSessionChallenge(): string {
 }
 
 export function useFeatureFlags() {
-  const NORMAL_FEATURES = ['tutorial', 'story', 'doorLock'];
+  const NORMAL_FEATURES = ['tutorial', 'story', 'doorLock', 'date'];
   const [features, setFeatures] = useState<string[]>(NORMAL_FEATURES);
 
   useEffect(() => {
