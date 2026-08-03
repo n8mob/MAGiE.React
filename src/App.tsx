d17156e71b4392a84e315f6b9694d7daee8e7241
Nate Grigg <n8mob@nategrigg.com>
2026-08-03 16:55:18 -0600

    Move chocolate2 routes inside chocolate feature flag

--- src/App.tsx
+++ src/App.tsx
@@ -304,11 +304,6 @@ function App() {
         <Route path="/bigGame/:categoryIndex/levels/:levelNumber/puzzles/:puzzleIndex"
                element={<LevelPlay menuName="bigGame" />} />
       </>)}
-      <Route path="/chocolate2" element={<MenuBrowser menuName="chocolate2" />} />
-      <Route path="/chocolate2/:categoryIndex" element={<CategoryBrowser menuName="chocolate2" />} />
-      <Route path="/chocolate2/:categoryIndex/levels/:levelNumber" element={<LevelBrowser menuName="chocolate2" />} />
-      <Route path="/chocolate2/:categoryIndex/levels/:levelNumber/puzzles/:puzzleIndex"
-             element={<LevelPlay menuName="chocolate2" />} />
       {features.includes('chocolate') && (<>
         {/* MENU_NAME_MAP aliases "chocolate" to the mall's API menu, so these
             routes browse mall content while links stay under /chocolate. */}
@@ -318,6 +313,11 @@ function App() {
         <Route path="/chocolate/:categoryIndex/levels/:levelNumber" element={<LevelBrowser menuName="chocolate" />} />
         <Route path="/chocolate/:categoryIndex/levels/:levelNumber/puzzles/:puzzleIndex"
                element={<LevelPlay menuName="chocolate" asChocolate={true} />} />
+        <Route path="/chocolate2" element={<MenuBrowser menuName="chocolate2" />} />
+        <Route path="/chocolate2/:categoryIndex" element={<CategoryBrowser menuName="chocolate2" />} />
+        <Route path="/chocolate2/:categoryIndex/levels/:levelNumber" element={<LevelBrowser menuName="chocolate2" />} />
+        <Route path="/chocolate2/:categoryIndex/levels/:levelNumber/puzzles/:puzzleIndex"
+               element={<LevelPlay menuName="chocolate2" />} />
       </>)}
       {features.includes('mall') && (<>
         <Route path="/mall" element={<MenuBrowser menuName="mall" />} />
