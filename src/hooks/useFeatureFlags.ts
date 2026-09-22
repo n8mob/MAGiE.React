0a219f27a52e78ed0f91603ea19fc598de7d4c4e
Nate Grigg <bluffdale@nategrigg.com>
2026-09-22 15:28:59 -0600

    Add 'mall' to default features for testing dual-channel features.

--- src/hooks/useFeatureFlags.ts
+++ src/hooks/useFeatureFlags.ts
@@ -27,7 +27,7 @@ function getOrCreateSessionChallenge(): string {
 }
 
 export function useFeatureFlags() {
-  const NORMAL_FEATURES = ['tutorial', 'doorLock', 'date', 'chocolate'];
+  const NORMAL_FEATURES = ['tutorial', 'doorLock', 'date', 'chocolate', 'mall'];
   const [features, setFeatures] = useState<string[]>(NORMAL_FEATURES);
 
   useEffect(() => {
